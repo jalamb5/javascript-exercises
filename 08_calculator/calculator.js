1e7e37a9f96a0257faf7044e94de8283cd410046
Justin Lamb <justinlamb@me.com>
2022-02-24 11:40:59 -0800

    completed calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(one, two) {
+	return one + two;
 };
 
-const subtract = function() {
-	
+const subtract = function(one, two) {
+  return one - two;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let value = 0;
+	for (i = 0; i < array.length; i++) {
+    value = add(value, array[i]);
+  }
+  return value;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let value = 1;
+  for (i = 0; i < array.length; i++) {
+    value = value * array[i];
+  }
+  return value;
 };
 
-const power = function() {
-	
+const power = function(one, two) {
+	return one ** two;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let array = []
+	for (i = num; i > 0; i--) {
+    array.push(i);
+  }
+  return multiply(array);
 };
 
 // Do not edit below this line
