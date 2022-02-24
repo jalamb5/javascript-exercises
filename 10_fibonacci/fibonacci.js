b51442538a0eca799e2ba3fe0ac08cc8a46636d4
Justin Lamb <justinlamb@me.com>
2022-02-24 12:59:00 -0800

    completed fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num < 0) {
+        return "OOPS";
+    };
+    let sequence = [1, 1,];
+    for (i = 1; i < num; i++) {
+        sequence.push(sequence[i] + sequence[i - 1])
+    }
+    return sequence[num - 1];
 };
 
 // Do not edit below this line
