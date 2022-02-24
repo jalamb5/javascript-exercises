fccd08c4d2b4c8d7df2bb59d65bc20261d653b14
Justin Lamb <justinlamb@me.com>
2022-02-24 12:31:04 -0800

    complete palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
-
+const palindromes = function(string) {
+    let newstring = string.toLowerCase();
+    let array = newstring.match(/[a-z]/g);
+    let yarra = [...array];
+    yarra.reverse();
+    let isPalindrome = false;
+    for (i = 0; i < array.length; i++) {
+        if (array[i] === yarra[i]) {
+            isPalindrome = true;
+        }
+        else {
+            isPalindrome = false;
+            break;
+        }
+    }
+    return isPalindrome;
 };
 
 // Do not edit below this line
